cb227a4bfb583a6a30fb1ee6fc0b9d2c2a4a7989
Juan Francisco Correas <jfcorreas@gmail.com>
2014-05-07 22:59:22 +0200

    Rutas con Express y vistas con Jade

--- app.js
+++ app.js
@@ -2,15 +2,14 @@
 
 var mongoose = require('mongoose');
 var express = require('express');
+var routes = require('./routes');
 
 mongoose.connect('mongodb://localhost', function(err) {
 	if (err) throw err;
 	console.log('Connected to MongoDB')
 
 	var app = express();
-	app.get('/', function (req, res) {
-		res.send(200, 'Hello mongoose blog');
-	})
+	routes(app);
 
 	app.listen(3000, function() {
 		console.log('Now listening on http://localhost:3000');
---
+++ routes/index.js
@@ -0,0 +1,9 @@
+'use strict'
+
+module.exports = function(app) {
+
+	// home page
+	app.get('/', function(req,res) {
+		res.render('home.jade');
+	})
+}
